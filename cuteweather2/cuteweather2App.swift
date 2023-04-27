2fb7d8b5c2d9ab47927c6a842820f94e8c334d3b
Justin Brady <justin@domain17.net>
2023-04-27 15:28:08 -0700

    Initial Commit

---
+++ cuteweather2/cuteweather2App.swift
@@ -0,0 +1,17 @@
+//
+//  cuteweather2App.swift
+//  cuteweather2
+//
+//  Created by Justin Brady on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct cuteweather2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
